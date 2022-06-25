bb5a16834684489cea1046aeccc171faf8ed41e3
Sim0nHack <simonhack.wdpt22@protonmail.com>
2022-06-25 17:03:31 +0200

    The whole lab

--- index.js
+++ index.js
@@ -15,9 +15,59 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => { // Connected to an empty database
+    return Recipe.create({
+      title: "Poutine",
+      level: "Easy",
+      ingredients: ["Potatoes", "Curd cheese", "Gravy sauce"],
+      cuisine: "Canadian",
+      dishType: "Fast and Fat",
+      image: "https://producteurslaitiersducanada.ca/sites/default/files/styles/recipe_image/public/image_file_browser/conso_recipe/autumn-vegetable-poutine.jpg.jpeg?itok=ToeXDmDC",
+      duration: 15,
+      creator: "Jean-Francis Poutine",
+      // created: new Date(Date.now())
+    })
+  })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);  // Consumes an array of recipes
+    //Recipe.create(data);    // Seems to do the same (but we also can use validators in the schema)
+  })
+  .then(() => {
+    let rigatoniId;
+
+    return Recipe.find({ title: "Rigatoni alla Genovese" }, "_id")
+      .then(docs => {
+        // rigatoniId = docs[0]._id.toString()
+        rigatoniId = docs[0].id;
+
+        Recipe.findOneAndUpdate({ _id: rigatoniId }, { duration: 100 })
+          .then(() => console.log("Update successful"))
+          .catch(error => console.log(`Update failed: ${error}`))
+      })
+      .catch(error => console.log(`Unable to get Rigatoni id: ${error}`))
+  })
+  .then(() => {
+    let carrotCakeId;
+
+    return Recipe.find({ title: "Carrot Cake" }, "_id")
+      .then(docs => {
+        // carrotCakeId = docs[0]._id.toString()
+        carrotCakeId = docs[0].id;
+
+        Recipe.deleteOne({ _id: carrotCakeId })
+          .then(() => console.log("Delete one successful"))
+          .catch(error => console.log(`Delete one failed: ${error}`))
+          .finally(() => { 
+            mongoose.connection.close();
+            console.log("Connection closed");
+          });
+      })
+      .catch(error => console.log(`Unable to get Rigatoni id: ${error}`))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+// Close connection : mongoose.connection.close();
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: String,
+  level: String,
+  ingredients: [ String ],
+  cuisine: String,
+  dishType: String,
+  image : {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: String,
+  created: Date 
+}, { timestamps: true });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
